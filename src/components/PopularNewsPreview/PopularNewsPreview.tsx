9bf115bb6b352da491112bcb302dcd1a37116135
Andrew <AndrewGaity@yandex.by>
2022-05-19 12:29:12 +0300

    refactoring: CSS-window, popular news preview

--- src/components/PopularNewsPreview/PopularNewsPreview.tsx
+++ src/components/PopularNewsPreview/PopularNewsPreview.tsx
@@ -2,6 +2,31 @@ import { FC } from 'react'
 
 import style from './PopularNewsPreview.module.scss'
 
-export const PopularNewsPreview: FC = () => (
-  <div className={style.container}>popular news</div>
+import { NewsType } from 'store/types'
+
+type PopularNewsPreviewPropsType = {
+  setCurrentNews: (newsId: number) => void
+  news: NewsType[]
+}
+
+export const PopularNewsPreview: FC<PopularNewsPreviewPropsType> = ({
+  news,
+  setCurrentNews,
+}) => (
+  <div className={style.container}>
+    <h3>Популярные статьи</h3>
+    {news.map(item => (
+      <div
+        tabIndex={0}
+        role="button"
+        key={item.id}
+        className={style.news}
+        onClick={() => setCurrentNews(item.id)}
+      >
+        <h4> {item.subtitle_1}</h4>
+        <span>{item.full_text_1}</span>
+        <div>Читать далее</div>
+      </div>
+    ))}
+  </div>
 )
--- src/pages/CurrentNews/CurrentNews.tsx
+++ src/pages/CurrentNews/CurrentNews.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react'
+import { FC, useCallback, useEffect } from 'react'
 
 import { useSelector } from 'react-redux'
 
@@ -9,11 +9,34 @@ import { NewsPreview } from './NewsPreview/NewsPreview'
 
 import { Navigation, NavLinkComponent, PopularNewsPreview } from 'components'
 import { Path } from 'enums/enums'
-import { selectorCurrentNews, selectorIsAdminMode } from 'store/selectors'
+import { setCurrentNewsAC } from 'store/reducers'
+import {
+  selectorCurrentNews,
+  selectorIsAdminMode,
+  selectorPartSearchNews,
+} from 'store/selectors'
+import { useAppDispatch } from 'store/store'
+import { addNewsViewsValueTC } from 'store/thunks'
+import { getPopularNewsTC } from 'store/thunks/popular_news_thunks'
 
 export const CurrentNews: FC = () => {
+  const dispatch = useAppDispatch()
+
   const news = useSelector(selectorCurrentNews)
+  const popularNews = useSelector(selectorPartSearchNews) // пока редьюсер свободен туда сетаются популярные новости
   const isAdmin = useSelector(selectorIsAdminMode)
+
+  useEffect(() => {
+    dispatch(getPopularNewsTC())
+  }, [])
+
+  const setCurrentNews = useCallback(
+    (newsId: number) => {
+      dispatch(setCurrentNewsAC(newsId))
+      dispatch(addNewsViewsValueTC(newsId))
+    },
+    [dispatch],
+  )
 
   return (
     <div className={style.container}>
@@ -21,7 +44,7 @@ export const CurrentNews: FC = () => {
       <Navigation />
       <div className={style.body}>
         <NewsBody news={news} isAdmin={isAdmin} />
-        <PopularNewsPreview />
+        <PopularNewsPreview news={popularNews} setCurrentNews={setCurrentNews} />
       </div>
       <div>
         <NewsPreview />
