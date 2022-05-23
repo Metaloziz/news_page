85fdbaa3653c5727c1fae82d96b22728a97c5e5a
Andrew <andrewgaity@yandex.by>
2022-05-23 10:15:37 +0300

    feat: convert html from JSON to html into React

--- src/components/Footer/CoursePreview/CoursePreview.tsx
+++ src/components/Footer/CoursePreview/CoursePreview.tsx
@@ -4,10 +4,20 @@ import style from './CoursePreview.module.scss'
 
 import { CoursesType } from 'api/commonDataRequests'
 
-export const CoursePreview: FC<{ courses: CoursesType }> = ({ courses }) => (
-  <div className={style.container}>
-    <h1>{courses.name_course}</h1>
-    <div>{courses.description_course}</div>
-    <span>подробнее</span>
-  </div>
-)
+export const CoursePreview: FC<{ courses: CoursesType }> = ({ courses }) => {
+  const bufferDiv = document.createElement('div')
+  bufferDiv.innerHTML = courses.description_course
+  const text = Array.from(bufferDiv.children).map(el => (
+    <li key={el.textContent}>{el.textContent}</li>
+  ))
+  return (
+    <div className={style.container}>
+      <h2>{courses.name_course}</h2>
+      <div>
+        <h3>После прохождения курса ты сможешь:</h3>
+        <div>{text}</div>
+      </div>
+      <span>подробнее</span>
+    </div>
+  )
+}
