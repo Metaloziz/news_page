95ba0b33d37692297a4bac46adf5f00fcb58a467
Andrew Gaityukevich <andrewgaity@yandex.by>
2022-05-23 20:18:36 +0300

    refactoring: views, add eye icon

--- src/components/NewsContainer/NewsPreview/NewsPreview.tsx
+++ src/components/NewsContainer/NewsPreview/NewsPreview.tsx
@@ -1,9 +1,10 @@
 /* eslint-disable */
-import { FC, memo } from 'react'
+import { FC, memo } from "react";
 
-import style from './News.module.scss'
-import { NewsType } from 'store/types'
-import { DeleteButton } from 'components/DeleteButton/DeleteButton'
+import style from "./News.module.scss";
+import { NewsType } from "store/types";
+import { DeleteButton } from "components/DeleteButton/DeleteButton";
+import eye from "assets/images/common/eye.svg";
 
 type NewsPropsType = {
   data: NewsType
@@ -17,25 +18,28 @@ export const NewsPreview: FC<NewsPropsType> = memo(
   ({
      data: { id, date, image_1, name, full_text_1, views },
      setCurrentNews,
-     newsRouteHandle, deleteNews, isAdmin,
+     newsRouteHandle, deleteNews, isAdmin
    }) => {
 
     const openCurrentNews = (): void => {
-      setCurrentNews(id)
-      newsRouteHandle()
-    }
+      setCurrentNews(id);
+      newsRouteHandle();
+    };
 
     return (
       <div className={style.container}>
         {isAdmin && <DeleteButton onClick={() => deleteNews(id)} />}
         <div className={style.body} onClick={openCurrentNews}>
           <div>
-            <img alt='ava' src={image_1} />
+            <img alt="ava" src={image_1} />
           </div>
           <div className={style.description}>
             <div className={style.date_and_view}>
               <div>{date}</div>
-              <div>просмотров: {views}</div>
+              <div className={style.eye}>
+                <img alt={"eye"} src={eye} />
+                <div>{views}</div>
+              </div>
             </div>
             <h3>{name}</h3>
             <div className={style.text}>{full_text_1}</div>
@@ -43,6 +47,6 @@ export const NewsPreview: FC<NewsPropsType> = memo(
           <span>Читать далее</span>
         </div>
       </div>
-    )
-  },
-)
+    );
+  }
+);
