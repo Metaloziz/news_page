f896715ef56bff151ab502942a641358e7305484
Andrew <andrewgaity@yandex.by>
2022-05-30 13:52:11 +0300

    refactoring: Mobile-css, header

--- src/components/RoutesComponent/RoutesComponent.tsx
+++ src/components/RoutesComponent/RoutesComponent.tsx
@@ -7,14 +7,12 @@ import { CreateNewsPage, CreateSectionPage, CurrentNews, MainPage } from 'pages'
 import { ChangeNewsPage } from 'pages/ChangeNewsPage/ChangeNewsPage'
 
 export const RoutesComponent: FC = () => (
-  <div>
-    <Routes>
-      <Route path={Path.DEFAULT} element={<Navigate to={Path.MAIN} />} />
-      <Route path={Path.MAIN} element={<MainPage />} />
-      <Route path={Path.CURRENT_NEWS} element={<CurrentNews />} />
-      <Route path={Path.CREATE_NEWS} element={<CreateNewsPage />} />
-      <Route path={Path.CREATE_SECTION} element={<CreateSectionPage />} />
-      <Route path={Path.CHANGE_NEWS} element={<ChangeNewsPage />} />
-    </Routes>
-  </div>
+  <Routes>
+    <Route path={Path.DEFAULT} element={<Navigate to={Path.MAIN} />} />
+    <Route path={Path.MAIN} element={<MainPage />} />
+    <Route path={Path.CURRENT_NEWS} element={<CurrentNews />} />
+    <Route path={Path.CREATE_NEWS} element={<CreateNewsPage />} />
+    <Route path={Path.CREATE_SECTION} element={<CreateSectionPage />} />
+    <Route path={Path.CHANGE_NEWS} element={<ChangeNewsPage />} />
+  </Routes>
 )
