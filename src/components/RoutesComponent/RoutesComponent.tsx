f7f84ab52c3240d4a73915d84330d26d21607092
Andrew <andrewgaity@yandex.by>
2022-06-03 16:26:02 +0300

    feat: added a page 404

--- src/components/RoutesComponent/RoutesComponent.tsx
+++ src/components/RoutesComponent/RoutesComponent.tsx
@@ -2,6 +2,7 @@ import { FC } from 'react'
 
 import { Navigate, Route, Routes } from 'react-router-dom'
 
+import { Component404 } from 'components/commonComponents/Component404/Component404'
 import { Path } from 'enums'
 import {
   ChangeNewsPage,
@@ -19,5 +20,6 @@ export const RoutesComponent: FC = () => (
     <Route path={Path.CREATE_NEWS} element={<CreateNewsPage />} />
     <Route path={Path.CREATE_SECTION} element={<CreateSectionPage />} />
     <Route path={Path.CHANGE_NEWS} element={<ChangeNewsPage />} />
+    <Route path="*" element={<Component404 />} />
   </Routes>
 )
---
+++ src/components/commonComponents/Component404/Component404.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react'
+
+import style from './Component404.module.scss'
+
+export const Component404: FC = () => (
+  <div className={style.container}>
+    <span>error 404</span> <span>page not found</span>
+  </div>
+)
